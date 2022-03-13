b6239b1f4d19c435682a543193dce42a82cb598e
khassangali <hasangaly@gmail.com>
2022-03-13 15:28:01 -0500

    Lab09

--- src/main/java/edu/mum/cs/cs525/labs/lab9/Application.java
+++ src/main/java/edu/mum/cs/cs525/labs/lab9/Application.java
@@ -2,10 +2,14 @@ package edu.mum.cs.cs525.labs.lab9;
 
 public class Application {
     public static void main(String[] args) throws InterruptedException {
-        ComplextTaskService task = ProxyUtility.getProxy();
+        ComplextTaskService task1 = ProxyUtility.getProxy(ComplexClass.class);
+        ComplextTaskService task2 = ProxyUtility.getProxy(MyNextComplextClass.class);
         System.out.println("Hi I am working, not waiting the proxy instantiation");
-        ComplextTaskService task2 = ProxyUtility.getProxy2();
-        task.veryNotComplicatedTask();
-        task2.veryNotComplicatedTask();
+//        task1.veryComplicatedTask();
+//        task2.veryComplicatedTask();
+        for (int i = 0; i < 12; i++) {
+            Thread.sleep(1000);
+            System.out.println("Doing our tasks " + i);
+        }
     }
 }
--- src/main/java/edu/mum/cs/cs525/labs/lab9/ProxyUtility.java
+++ src/main/java/edu/mum/cs/cs525/labs/lab9/ProxyUtility.java
@@ -4,11 +4,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 public class ProxyUtility {
-    public static <ComplexClassService> ComplexClassService getProxy(){
-        return (ComplexClassService) Proxy.newProxyInstance(ComplextTaskService.class.getClassLoader(), ComplexClass.class.getInterfaces(), new DynamicProxy());
-    }
-
-    public static <ComplexClassService> ComplexClassService getProxy2(){
-        return (ComplexClassService) Proxy.newProxyInstance(ComplextTaskService.class.getClassLoader(), MyNextComplextClass.class.getInterfaces(), new DynamicProxy());
+    public static <T> T getProxy(Class<T> target){
+        return (T) Proxy.newProxyInstance(target.getClassLoader(), target.getInterfaces(), new DynamicProxy());
     }
 }
